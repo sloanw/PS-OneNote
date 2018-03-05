7e55cbf2051dd90408ad51f5411e72408ee670e6
Sloan Whitney <swhitney@efront.com>
2018-03-05 09:30:35 -0500

    total rewrite with PS best practices

--- OneNoteFunctions.psm1
+++ OneNoteFunctions.psm1
@@ -1,170 +1,218 @@
-function New-OneNoteSection {
+Function Get-OneNote {
 	[CmdletBinding()]
-	Param(
-		[Parameter(ValueFromPipeline = $True)]
-		[xml]$Hierarchy,
-		[string]$Section
+	Param (
 	)
-
-	If ($Hierarchy -eq $null) {
-		$Hierarchy = Get-OneNoteHierarchy;
-	}
-
-	[System.Xml.XmlNode]$Root = $Hierarchy.ChildNodes.Item(1);
-	$NewSection = $Hierarchy.CreateElement('one', 'Section', $Root.NamespaceURI);
-	$NewSection.SetAttribute('name', $Section);
 	
-	Return $NewSection;
+	Return New-Object -ComObject OneNote.Application;
 }
 
-function Get-OneNoteHierarchy {
+Function Get-OneNoteHierarchy {
 	[CmdletBinding()]
-	Param()
+	Param(
+		# Hierarchy Level to return
+		[Microsoft.Office.Interop.OneNote.HierarchyScope]
+		$Level
+	)
+	
+	Begin {
+		$OneNote = Get-OneNote;
+		$Hierarchy = "";
 
-	$OneNote = New-Object -ComObject OneNote.Application;
-	$Hierarchy = "";
-	$OneNote.GetHierarchy("", [Microsoft.Office.Interop.OneNote.HierarchyScope]::hsSections, [ref] $Hierarchy);
+		If ($Level -eq $null ) {
+			$Level = [Microsoft.Office.Interop.OneNote.HierarchyScope]::hsSections;
+		}
+	}
+	
+	Process {
+		$OneNote.GetHierarchy("", $Level, [ref] $Hierarchy);
+	}
+	
+	End {
+		Return [xml] $Hierarchy;
+	}
+}
 
-	Return [xml] $Hierarchy;
+function Get-HierarchyNodes {
+	[CmdletBinding()]
+	param (
+		# Hierarchy Level to search
+		[Parameter(Mandatory = $True)]
+		[Microsoft.Office.Interop.OneNote.HierarchyScope]
+		$Level,
+		# Node XPath
+		[Parameter(Mandatory = $True)]
+		[string]
+		$XPath
+	)
+	
+	begin {
+		$Hierarchy = Get-OneNoteHierarchy -Level $Level;
+		
+		$Namespace = New-Object System.Xml.XmlNamespaceManager($Hierarchy.NameTable);
+		[System.Xml.XmlNode]$Root = $Hierarchy.ChildNodes.Item(1);
+		$Namespace.AddNamespace("one", $Root.NamespaceURI);
+	}
+	
+	process {
+		[System.Xml.XmlElement[]]$Nodes = $Hierarchy.SelectNodes($XPath, $Namespace);
+	}
+	
+	end {
+		Return $Nodes;
+	}
 }
 
 function Set-OneNoteHierarchy {
 	[CmdletBinding()]
-	Param(
-		[Parameter(Mandatory = $True,ValueFromPipeline = $True)]
-		[xml]$Hierarchy
-	)
-
-	$OneNote = New-Object -ComObject OneNote.Application;
-	$OneNote.UpdateHierarchy($Hierarchy.OuterXml);
-}
-
-function Add-OneNoteOutline {
-	[CmdletBinding()]
 	param (
-		[Parameter(Mandatory = $True)]
-		[string]
-		$PageID
+		# XML to be inserted into the OneNote Hierarchy
+		[Parameter(Mandatory = $True, ValueFromPipeline = $True)]
+		[xml]
+		$Hierarchy
 	)
 	
 	begin {
+		$OneNote = Get-OneNote;
 	}
 	
 	process {
+		$OneNote.UpdateHierarchy($Hierarchy.OuterXml);
 	}
 	
 	end {
 	}
 }
 
-function Add-OneNotePage {
+Function Get-OneNoteNotebook {
 	[CmdletBinding()]
-	Param(
-		[string]$SectionID,
-		[string]$PageTitle
+	Param (
+		# Notebook name
+		[String[]]
+		$Notebook
 	)
-	$OneNote = New-Object -ComObject OneNote.Application;
+	
+	Begin {
+		$Lvl = [Microsoft.Office.Interop.OneNote.HierarchyScope]::hsNotebooks;
+		$XPath = "one:Notebooks/one:Notebook";
+		[System.Xml.XmlElement[]]$AllNotebooks = Get-HierarchyNodes -Level $Lvl -XPath $XPath;
+		[System.Xml.XmlElement[]]$Notebooks = @();
+	}
+	
+	Process {
+		ForEach ($Name In $Notebook) {
+			$Notebooks += ($AllNotebooks | Where-Object { $_.Name -eq $Name });
+		}
+	}
+	
+	End {
+		If ( $Notebook.Count -gt 0 ) {
+			Return $Notebooks;
+		}
+		Else {
+			Return $AllNotebooks;
+		}
+	}
+}
 
-	$PageID = $null;
-	$OneNote.CreateNewPage($SectionID, [ref] $PageID);
+Function Get-OneNoteSection {
+	[CmdletBinding()]
+	Param (
+		# Notebook to search
+		[Parameter(Mandatory = $True)]
+		[string]
+		$Notebook,
+		# Name of the section to return
+		[Parameter(ValueFromPipeline = $True)]
+		[string[]]
+		$SectionName,
+		# Name of the section group to look In
+		[string]
+		$SectionGroup
+	)
 
-	If ($PageTitle -ne $null) {
-		[xml]$pageXML = Get-OneNotePageContents -PageID $PageID
-		Set-OneNotePageTitle -PageXML $pageXML -PageTitle $PageTitle
-		Set-OneNotePageContents -PageXML $pageXML
+	Begin {
+		$Lvl = [Microsoft.Office.Interop.OneNote.HierarchyScope]::hsSections;
+		$XPath = "/one:Notebooks/one:Notebook[@name='$Notebook']/";
+		If ( $SectionGroup -ne $null -and $SectionGroup.Length -gt 0) {
+			$XPath += "one:SectionGroup[@name='$SectionGroup']/"
+		}
+		$XPath += "one:Section";
+
+		[System.Xml.XmlElement[]]$AllSections = Get-HierarchyNodes -Level $Lvl -XPath $XPath;
+		[System.Xml.XmlElement[]]$Sections = @();
 	}
 
+	Process {
+		ForEach ($Name In $SectionName) {
+			$Sections += ($AllSections | Where-Object { $_.Name -eq $Name });
+		}
+	}
 
-	Return $PageID;
+	End {
+		If ( $SectionName.Count -gt 0 ) {
+			Return $Sections;
+		} else {
+			Return $AllSections;
+		}
+	}
+}
+
+Function Add-OneNotePage {
+	[CmdletBinding()]
+	Param (
+		# ID of the Section our page will be created In
+		[Parameter(Mandatory = $True)]
+		[string]
+		$SectionID,
+		# Title of the page to be created
+		[Parameter(ValueFromPipeline = $True)]
+		[string[]]
+		$PageTitle
+	)
+	
+	Begin {
+		$OneNote = Get-OneNote;
+
+		[string[]]$PageIDs = @();
+	}
+	
+	Process {
+		ForEach ($Title In $PageTitle) {
+			[string]$PageID = $null;
+			$OneNote.CreateNewPage($SectionID, [ref] $PageID);
+			$PageIDs += $PageID;
+		}
+	}
+	
+	End {
+		Return $PageIDs
+	}
 }
 
 function Get-OneNotePageContents {
 	[CmdletBinding()]
-	Param(
-		[Parameter(ValueFromPipeline = $True)]
-		[string]$PageID
-	)
-	$OneNote = New-Object -ComObject OneNote.Application;
-
-	$PageXML = $null;
-	$OneNote.GetPageContent($PageID, [ref] $PageXML);
-
-	Return $PageXML;
-}
-
-function Set-OneNotePageContents {
-	[CmdletBinding()]
-	Param(
-		[Parameter(ValueFromPipeline = $True)]
-		$PageXML
-	)
-
-	If ($pageXML.GetType() -eq [System.Xml.XmlDocument]) {
-		$PageXML = $PageXML.OuterXML;
-	}
-
-	$OneNote = New-Object -ComObject OneNote.Application;
-	$OneNote.UpdatePageContent($PageXML);
-}
-
-function Get-OneNoteSection {
-	[CmdletBinding()]
-	Param(
-		[Parameter(ValueFromPipeline = $True)]
-		[xml]$Hierarchy,
-		[string]$Notebook,
-		[string]$SectionGroup,
-		[string]$SectionName
-	)
-
-	If ($Hierarchy -eq $null) {
-		$Hierarchy = Get-OneNoteHierarchy;
-	}
-
-	$Group = Get-OneNoteSectionGroup -Hierarchy $h -Notebook $Notebook -SectionGroup $SectionGroup;
-
-	[System.Xml.XmlNode] $Section = $Group.Section | Where-Object { $_.name -eq $SectionName };
-
-	Return $Section
-}
-
-function Set-OneNotePageTitle {
-	[CmdletBinding()]
-	Param(
+	param (
+		# ID of page to return
 		[Parameter(Mandatory = $True, ValueFromPipeline = $True)]
-		[xml]$PageXML,
-		[string]$PageTitle
+		[string[]]
+		$PageID
 	)
-
-	[System.Xml.XmlNode]$Root = $PageXML.GetElementsByTagName("one:Page")[0];
-	$Namespace = New-Object System.Xml.XmlNamespaceManager($PageXML.NameTable);
-	$Namespace.AddNamespace("one", $root.NamespaceURI);
-	$XPath = "/one:Page/one:Title/one:OE/one:T";
 	
-	[System.Xml.XmlNode]$TitleNode = $PageXML.SelectSingleNode($XPath, $Namespace);
-	$TitleNode.InnerXml = "<![CDATA[$PageTitle]]>";
+	begin {
+		$OneNote = Get-OneNote;
 
-	Return $PageXML;
-}
-
-function Get-OneNoteSectionGroup {
-	[CmdletBinding()]
-	Param(
-		[Parameter(ValueFromPipeline = $True)]
-		[xml]$Hierarchy,
-		[string]$Notebook,
-		[string]$SectionGroup
-	)
-
-	If ($Hierarchy -eq $null) {
-		$Hierarchy = Get-OneNoteHierarchy;
+		[System.Xml.XmlDocument[]]$Pages = @();
 	}
-
-	$Namespace = New-Object System.Xml.XmlNamespaceManager($Hierarchy.NameTable);
-	[System.Xml.XmlNode]$Root = $Hierarchy.ChildNodes.Item(1);
-	$Namespace.AddNamespace("one", $Root.NamespaceURI);
-
-	$XPath = "/one:Notebooks/one:Notebook[@name='$Notebook']/one:SectionGroup[@name='$SectionGroup']";
-
-	Return [System.Xml.XmlNode] $Hierarchy.SelectSingleNode($XPath, $Namespace);
+	
+	process {
+		foreach ($ID in $PageID) {
+			[System.Xml.XmlDocument]$Page = $null;
+			$OneNote.GetPageContent($ID, [ref] $Page);
+			$Pages += $Page;
+		}
+	}
+	
+	end {
+		Return $Pages;
+	}
 }
